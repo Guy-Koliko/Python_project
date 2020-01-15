711d24887b8fbea46ebde6c7824b4b83a9cf65c1
Guy-Koliko <ubnt5050win@gmail.com>
2020-01-15 12:40:49 +0000

    My first python OOP code

---
+++ classes/RomanClass.py
@@ -0,0 +1,14 @@
+# importing the roman function
+
+from functions import roman_function as rn
+
+# building the Roman class
+class Roman:
+
+    # building the constructor for our class
+    def __init__(self,num):
+        self.num = num
+
+    # building the display object to display our numerals
+    def display(self):
+        print(rn.conVertNumber(self)," STAND FOR ", self)
---
+++ main/roman_main.py
@@ -0,0 +1,24 @@
+"""
+    This is a simple program that takes in a number
+    and shows the Roman numeral part of that number
+"""
+from classes import RomanClass as rn
+
+def inputs():
+    tn = int(input("Enter Your Number : "))
+    runprograme = rn.Roman.display(tn)
+    return (runprograme)
+
+def banner():
+    b ="""
+    *******************************************
+    *  WELCOME TO THE ROMAN NUMERAL CONVECTOR *
+    *******************************************
+"""
+    print(b)
+
+def _main():
+    banner()
+    inputs()
+
+_main()
